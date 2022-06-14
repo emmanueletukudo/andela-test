da9956bb535deeb1570d20b37ead71008a8b8948
Emmanuel Etukudo <emmanueletukudo@Emmanuels-MacBook-Pro.local>
2022-06-14 15:59:06 +0100

    Completed form submit task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [formValues, setFormValues] = useState([{ name: "", age: "", job: "" }]);
+  const [show, setShow] = useState(false);
+  const [result, setResult] = useState([]);
+
+  const handleChange = (i, e) => {
+    let newValues = [...formValues];
+    newValues[i][e.target.name] = e.target.value;
+    setFormValues(newValues);
+  }
+
+  const handleAdd = (e) => {
+    e.preventDefault()
+    setFormValues([...formValues, { name: "", age: "", job: "" }])
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setShow(true);
+    setResult(JSON.stringify(formValues));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+        <form onSubmit={handleSubmit} className='job-form'>
+          {!show && <div className='input-wrapper'>
+            {formValues.map((v, i) => (
+              <>
+                <input type="text" name="name" value={v.name} onChange={e => handleChange(i, e)} placeholder="name" /><br />
+                <input type="text" name="age" value={v.age} onChange={e => handleChange(i, e)} placeholder="age" /><br />
+                <input type="text" name="job" value={v.job} onChange={e => handleChange(i, e)} placeholder="job title" /><br />
+              </>
+            ))}
+
+            <button onClick={handleAdd}>Add</button>
+            <button onClick={handleSubmit}>Submit</button>
+          </div>}
+        </form>
+        <div>
+          {result}
+        </div>
+      </>
     </div>
   );
 }
